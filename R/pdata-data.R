f5896f1032561993faaeb4638c08ce3dd8bc08b7
Irene SL Zeng <ireneslzeng@users.noreply.github.com>
2018-04-01 21:07:03 +1200

    Add files via upload

--- R/pdata-data.R
+++ R/pdata-data.R
@@ -1,10 +1,12 @@
-#' pdata for examples and testthat()
-#' pdata has 7 variables and var1 is the response variable, var2 is a continuous explanatory variable
-#' treatment is another explanatory variable, miss and censor are indicator for missing and censored 
-#' geneid and sid represents gene id and subject id respectively.
-#' @docType data
+#'pdata for examples and testthat()
+#'pdata has 7 variables and var1 is the response variable, var2 is a continuous
+#'explanatory variable,
+#'treatment is another explanatory variable, miss and censor are indicator for 
+#'missing and censored,
+#'geneid and sid represents gene id and subject id respectively.
+#'@docType data
 #'
-#' @usage data(pdata)
+#'@usage data(pdata)
 #'
-#' @keywords datasets
+#'@keywords datasets
 "pdata"
